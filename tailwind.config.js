bb519c993b65b1cc53587aa3a63b8cbe43143f09
vidura-12 <viduranirmal195@gmail.com>
2025-05-09 18:35:34 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
   theme: {
     extend: {
       keyframes: {
@@ -28,20 +29,3 @@ module.exports = {
     },
   },
 };
-
-// Add these animation classes to your tailwind.config.js if not already there
-// module.exports = {
-//   theme: {
-//     extend: {
-//       animation: {
-//         shimmer: 'shimmer 2s infinite linear',
-//       },
-//       keyframes: {
-//         shimmer: {
-//           '0%': { backgroundPosition: '200% 0' },
-//           '100%': { backgroundPosition: '-200% 0' },
-//         },
-//       },
-//     },
-//   },
-// }
